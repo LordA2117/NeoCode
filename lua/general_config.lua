1d6991f0388db6ba7624c49160ee6e3fc57faac8
LordA2117 <88305634+LordA2117@users.noreply.github.com>
2024-09-29 23:47:21 +0530

    Code documentation

--- lua/general_config.lua
+++ lua/general_config.lua
@@ -3,6 +3,8 @@ vim.cmd(':set number')
 vim.cmd(':set shiftwidth=2')
 vim.cmd(':set tabstop=2')
 vim.cmd('set cursorline')
+
+-- Config on windows for ToggleTerm. Do not use this if you're on linux
 vim.opt.shell = 'powershell.exe'
 vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
 vim.opt.shellxquote = ''
